aa8a8aa3655f0849e2b91570e15e0eba5971b1a0
luis petek <lmpetek@gmail.com>
2024-09-19 14:47:46 -0300

    clean code

--- get_url.py
+++ get_url.py
@@ -2,13 +2,14 @@ import requests
 import json
 
 try:
-    id = input("Enter the policy ID: ")
-    # id = "2"
+    id = input("Ingrese el ID de la poliza: ")
     url = "https://soter-premiar2-dev.herokuapp.com/api/v1/policies/{0}.json".format(id)
     r = requests.get(url)
     print("HTML:\n", r.text)
     datos = json.loads(r.text)
-    print("json", datos['id'], datos['taker_name'])
+    print("ID tomador", datos['id'], datos['taker_name'])
+    print("asegurado", datos['insured_name'])
+    print("poliza", datos['policy_number'])
 except:
     print(
         "Invalid URL or some error occured while making the GET request to the specified URL"
